8bf0257e42131fae2305e64a1261400eea938e48
Flavio Santos <flavio.pasantos@outlook.com>
2025-02-06 16:38:40 -0300

    feat: adjust, change for legacy code

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,27 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_Bwrz2JGc.mjs';
+import { manifest } from './manifest_D7Kk2KUl.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "5a709e33-459b-472b-bcc1-63278e8d3821",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +10,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  output: "server",
+  adapter: vercel(),
 });
