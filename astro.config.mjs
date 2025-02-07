9b0d3588c905b7f439a2ef98008cb903b2928ee1
Flavio Santos <flavio.pasantos@outlook.com>
2025-02-06 21:42:03 -0300

    feat: apply

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   vite: {
