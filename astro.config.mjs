04a8eed22bc68b7df5ac855c2efa389755e9f18a
Flavio Santos <flavio.pasantos@outlook.com>
2025-02-06 21:46:12 -0300

    feat: apply

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
+  output: "server",
+  adapter: netlify(),
 });
