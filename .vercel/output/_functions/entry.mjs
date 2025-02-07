7b56c72cbcf67940a145adb4da1bb769555229ee
Flavio Santos <flavio.pasantos@outlook.com>
2025-02-06 21:20:13 -0300

    feat: apply alterations

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_BSwNWelv.mjs';
-import { manifest } from './manifest_D3OKYwWT.mjs';
+import { manifest } from './manifest_VuqjY869.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -18,7 +18,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "bb1aab92-2d8b-49bf-8f4f-06a2778edc38",
+    "middlewareSecret": "e90560fe-35c6-4083-aaa9-10ac697d8653",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
