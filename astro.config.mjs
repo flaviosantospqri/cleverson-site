1c07ab16366909fdb82a4090d48f3e0577394351
Flavio Santos <flavio.pasantos@outlook.com>
2025-02-06 21:36:00 -0300

    feat: apply

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
-  output: "server",
-  adapter: vercel(),
 });
